fe89efbd6072f3b5df05984fe79ffdae7e40f391
antoine <you@example.com>
2017-10-29 22:57:55 +0100

    cordova plugin wip

--- ts/platform/DriveAPICordova.ts
+++ ts/platform/DriveAPICordova.ts
@@ -1,7 +1,12 @@
 
 import { Utilitary, openLoad, RezoSave, IDriveAPI } from '../index';
+import { Rezo } from '../rezo';
+import * as $ from 'jquery';
+declare let window: {
+    plugins: any
+}
 
-export class DriveAPICordova implements IDriveAPI{
+export class DriveAPICordova implements IDriveAPI {
     CLIENT_ID: string;
     SCOPES: string[];
     faustFolder: string;
@@ -12,12 +17,37 @@ export class DriveAPICordova implements IDriveAPI{
     tempBlob: Blob;
     innerHTML: string;
     extension: string;
+    constructor() {
+
+    }
+
+
     checkAuth: () => void;
-    updateConnection: () => void;
+    updateConnection () {
+        this.listFolder();
+    };
     handleAuthResult: (authResult: any) => void;
-    handleAuthClick: () => void;
+    handleAuthClick() {
+        Rezo.isDriveConnected = true;
+        $("#saveBulle").css("display", "block");
+        $("#homeBulle").css("display", "block");
+        $("#driveBulle").css("display", "none");
+        $("#realtimeBulle").css("display", "block");
+        $('#loading').css("display", "none");        
+    };
     loadDriveApi: () => void;
-    listFolder: () => void;
+
+    listFolder() {
+        window.plugins.gdrive.fileList(
+            (res) => {
+                //the files are under res.flist;
+                console.log(res);
+            },
+            function (err) {
+                console.log(err);
+            }
+        );
+    };
     appendPre: (name: string, id: string, timeStamp: string, innerHTMLRef: string) => string;
     downloadFile: (file: any, callback: (rezoSave: any, title: string, timeStamp: number) => void) => void;
     getFile: (fileId: any, callback: any) => any;
